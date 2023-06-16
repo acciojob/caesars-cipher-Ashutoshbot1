3a8553b52b64e65489e75f2403aa18f2cb758888
Ashutoshbot1 <100514925+Ashutoshbot1@users.noreply.github.com>
2023-06-16 14:14:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function rot13(encodedStr) {
   // Only change code below this line
 	for(let code of encodedStr){
 
-		if(lookup[code]== undefine){
+		if(lookup[code]=== undefine){
 			decodedArr.push(code);
 		}
 		else{			
